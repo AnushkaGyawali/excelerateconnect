6d6f8604d14354f6adaba0c905003f13b9746dff
Aabidnabi <tantryinfo98@gmail.com>
2026-01-18 23:13:10 +0530

    feat: Update Gradle wrapper to version 8.7 and Android plugin to version 8.1.1; remove unused import in home_screen.dart; add VSCode settings for Java configuration

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import '../theme/app_theme.dart';
-import '../widgets/section_title.dart';
 
 class CategoryChip extends StatelessWidget {
   final String label;
